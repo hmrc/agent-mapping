7248302ad68f4bc9e3457bec74a53c88ae9667fe
Leigh Anderson <landerson@equalexperts.com>
2017-04-03 10:56:32 +0100

    [LA][APB-703] Add mapping repository.

---
+++ app/uk/gov/hmrc/agentmapping/repository/MappingRepository.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentmapping.repository
+
+import javax.inject.{Inject, Singleton}
+
+import play.api.libs.json.{Format, Json}
+import play.modules.reactivemongo.ReactiveMongoComponent
+import reactivemongo.api.indexes.Index
+import reactivemongo.api.indexes.IndexType.Ascending
+import reactivemongo.bson.BSONObjectID
+import uk.gov.hmrc.agentmapping.model.Arn
+import uk.gov.hmrc.domain.SaAgentReference
+import uk.gov.hmrc.mongo.ReactiveRepository
+import uk.gov.hmrc.mongo.json.ReactiveMongoFormats
+
+import scala.concurrent.{ExecutionContext, Future}
+
+case class Mapping(arn: String, saAgentReference: String)
+
+object Mapping extends ReactiveMongoFormats {
+  implicit val formats: Format[Mapping] = Json.format[Mapping]
+}
+
+@Singleton
+class MappingRepository @Inject()(mongoComponent: ReactiveMongoComponent) extends
+    ReactiveRepository[Mapping, BSONObjectID]("agent-mapping", mongoComponent.mongoConnector.db, Mapping.formats, ReactiveMongoFormats.objectIdFormats) {
+
+  override def indexes = Seq(
+    Index(Seq("arn" -> Ascending, "saAgentReference" -> Ascending), Some("arnAndAgentReference"), unique = true)
+  )
+
+  def createMapping(arn: Arn, saAgentReference: SaAgentReference)(implicit ec: ExecutionContext): Future[Unit] = {
+    insert(Mapping(arn.value, saAgentReference.value)).map(_ => ())
+  }
+}
---
+++ it/uk/gov/hmrc/agentmapping/repository/MappingRepositoryISpec.scala
@@ -0,0 +1,59 @@
+package uk.gov.hmrc.agentmapping.repository
+
+import reactivemongo.core.errors.DatabaseException
+import uk.gov.hmrc.agentmapping.model.Arn
+import uk.gov.hmrc.agentmapping.support.MongoApp
+import uk.gov.hmrc.domain.SaAgentReference
+import uk.gov.hmrc.play.test.UnitSpec
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class MappingRepositoryISpec extends UnitSpec with MongoApp {
+
+  val arn = Arn("ARN00001")
+  val saAgentReference = SaAgentReference("Ref0001")
+
+  def repo: MappingRepository = app.injector.instanceOf[MappingRepository]
+
+  override def beforeEach() {
+    super.beforeEach()
+    await(repo.ensureIndexes)
+  }
+
+  "createMapping" should {
+    "create a mapping" in {
+      await(repo.createMapping(arn, saAgentReference))
+
+      val result = await(repo.find())
+
+      result.size shouldBe 1
+      result.head.arn shouldBe arn.value
+      result.head.saAgentReference shouldBe saAgentReference.value
+
+    }
+
+    "not allow duplicate mappings to be created for the same ARN and SA Agent Reference" in {
+      await(repo.createMapping(arn, saAgentReference))
+
+      val e = intercept[DatabaseException] {
+        await(repo.createMapping(arn, saAgentReference))
+      }
+
+      e.getMessage() should include ("E11000")
+    }
+
+    "allow more than one SA Agent Reference to be mapped to the same ARN" in {
+      val saAgentReference1 = SaAgentReference("REF0002")
+      await(repo.createMapping(arn, saAgentReference))
+      await(repo.createMapping(arn, saAgentReference1))
+
+      val result = await(repo.find())
+
+      result.size shouldBe 2
+      result.head.arn shouldBe arn.value
+      result.head.saAgentReference shouldBe saAgentReference.value
+      result(1).arn shouldBe arn.value
+      result(1).saAgentReference shouldBe saAgentReference1.value
+    }
+  }
+}
