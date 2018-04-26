ef925cb0cb25bb4526fb934bdb5f5f4d8246f25c
Artur Opala <opala.artur@gmail.com>
2018-04-26 18:16:03 +0100

    [APB-2241][AO] after formatting

--- app/uk/gov/hmrc/agentmapping/controller/MappingController.scala
+++ app/uk/gov/hmrc/agentmapping/controller/MappingController.scala
@@ -16,20 +16,20 @@
 
 package uk.gov.hmrc.agentmapping.controller
 
-import javax.inject.{Inject, Singleton}
+import javax.inject.{ Inject, Singleton }
 import play.api.Logger
 import play.api.libs.json.Json.toJson
-import play.api.mvc.{Action, AnyContent, Request}
+import play.api.mvc.{ Action, AnyContent, Request }
 import reactivemongo.core.errors.DatabaseException
 import uk.gov.hmrc.agentmapping.audit.AuditService
 import uk.gov.hmrc.agentmapping.connector.DesConnector
 import uk.gov.hmrc.agentmapping.model.Service._
 import uk.gov.hmrc.agentmapping.model._
 import uk.gov.hmrc.agentmapping.repository._
-import uk.gov.hmrc.agentmtdidentifiers.model.{Arn, Utr}
+import uk.gov.hmrc.agentmtdidentifiers.model.{ Arn, Utr }
 import uk.gov.hmrc.auth.core.AuthProvider.GovernmentGateway
 import uk.gov.hmrc.auth.core._
-import uk.gov.hmrc.auth.core.retrieve.{Retrievals, _}
+import uk.gov.hmrc.auth.core.retrieve.{ Retrievals, _ }
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.HeaderCarrierConverter.fromHeadersAndSession
 import uk.gov.hmrc.play.bootstrap.controller.BaseController
@@ -37,11 +37,11 @@ import uk.gov.hmrc.play.bootstrap.controller.BaseController
 import scala.concurrent.Future
 
 @Singleton
-class MappingController @Inject()(
-                                   repositories: MappingRepositories,
-                                   desConnector: DesConnector,
-                                   auditService: AuditService,
-                                   val authConnector: AuthConnector)
+class MappingController @Inject() (
+  repositories: MappingRepositories,
+  desConnector: DesConnector,
+  auditService: AuditService,
+  val authConnector: AuthConnector)
   extends BaseController with AuthorisedFunctions {
 
   import auditService._
@@ -72,13 +72,13 @@ class MappingController @Inject()(
               Future.successful(Forbidden)
           }
       }.recoverWith {
-      case ex: NoActiveSession =>
-        Logger.warn("No active session whilst trying to create mapping", ex)
-        Future.successful(Unauthorized)
-      case ex: AuthorisationException =>
-        Logger.warn("Authorisation exception whilst trying to create mapping", ex)
-        Future.successful(Forbidden)
-    }
+        case ex: NoActiveSession =>
+          Logger.warn("No active session whilst trying to create mapping", ex)
+          Future.successful(Unauthorized)
+        case ex: AuthorisationException =>
+          Logger.warn("Authorisation exception whilst trying to create mapping", ex)
+          Future.successful(Forbidden)
+      }
   }
 
   private def captureIdentifiersAndAgentCodeFrom(enrolments: Enrolments, agentCodeOpt: Option[String]): Option[Set[Identifier]] = {
