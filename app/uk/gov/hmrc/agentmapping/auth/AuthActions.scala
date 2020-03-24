f5549d4aa022756f8efd8f92fd6c2de582394b97
jordanrowe <jordan.rowe@digital.hmrc.gov.uk>
2020-03-24 16:37:07 +0000

    [APB-4804][JR] Implement Basic Auth for terminations

--- app/uk/gov/hmrc/agentmapping/auth/AuthActions.scala
+++ app/uk/gov/hmrc/agentmapping/auth/AuthActions.scala
@@ -117,16 +117,28 @@ class AuthActions @Inject()(val authConnector: AuthConnector, cc: ControllerComp
     } catch { case _: Throwable => "" }
 
   def withBasicAuth(expectedAuth: BasicAuthentication)(body: => Future[Result])(
-    implicit request: Request[_]): Future[Result] =
+    implicit request: Request[_]): Future[Result] = {
     request.headers.get(HeaderNames.authorisation) match {
       case Some(basicAuthHeader(encodedAuthHeader)) =>
         decodeFromBase64(encodedAuthHeader) match {
-          case decodedAuth(username, password) if (BasicAuthentication(username, password) == expectedAuth) =>
-            body
-          case _ => Future successful Unauthorized
+          case decodedAuth(username, password) =>
+            if (BasicAuthentication(username, password) == expectedAuth) body
+            else {
+              Logger.warn(
+                "Authorization header found in the request but invalid username or password")
+              Future successful Unauthorized
+            }
+          case _ =>
+            Logger.warn(
+              "Authorization header found in the request but its not in the expected format")
+            Future successful Unauthorized
         }
-      case _ => Future successful Unauthorized
+      case _ =>
+        Logger.warn(
+          "No Authorization header found in the request for agent termination")
+        Future successful Unauthorized
     }
+  }
 
   def onlyStride(strideRole: String)(body: Request[AnyContent] => Future[Result])(
     implicit ec: ExecutionContext): Action[AnyContent] =
