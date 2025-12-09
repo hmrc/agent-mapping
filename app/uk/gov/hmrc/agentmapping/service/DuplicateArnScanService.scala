2291cc70b00aa3599189278885ce7cb2fe9baa78
Ian Walton <51008396+ian-1@users.noreply.github.com>
2025-12-09 15:27:53 +0000

    APB-10381 - Mapping data duplicate ARN Query

---
+++ app/uk/gov/hmrc/agentmapping/service/DuplicateArnScanService.scala
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentmapping.service
+
+import org.mongodb.scala.MongoCollection
+import org.mongodb.scala.bson.collection.immutable.Document
+import org.mongodb.scala.model.Accumulators
+import org.mongodb.scala.model.Aggregates
+import org.mongodb.scala.model.Filters
+import org.mongodb.scala.model.Projections
+
+import javax.inject.Inject
+import javax.inject.Singleton
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import play.api.Logging
+import uk.gov.hmrc.agentmapping.model.AgentReferenceMapping
+import uk.gov.hmrc.agentmapping.model.ArnCount
+import uk.gov.hmrc.agentmapping.repository.MappingRepositories
+import uk.gov.hmrc.mongo.lock.LockService
+import uk.gov.hmrc.mongo.lock.MongoLockRepository
+
+@Singleton
+class DuplicateArnScanService @Inject() (
+  mappingRepositories: MappingRepositories,
+  mongoLockRepository: MongoLockRepository
+)(implicit ec: ExecutionContext)
+extends Logging {
+
+  private val lockService = LockService(
+    mongoLockRepository,
+    lockId = "mapping-duplicate-arn-scan",
+    ttl = 10.minutes
+  )
+
+  def runDuplicateArnScanLocked(): Future[Unit] = lockService.withLock {
+    logger.info("Acquired lock for mapping-duplicate-arn-scan; starting duplicate ARN analysis")
+    runDuplicateArnScan()
+  }.map {
+    case Some(_) => logger.info("Duplicate ARN scan completed; lock released")
+    case None => logger.info("Duplicate ARN scan skipped (another instance already running)")
+  }
+
+  private[service] def findIdentifierArnCounts(collection: MongoCollection[AgentReferenceMapping])(implicit ec: ExecutionContext): Future[Seq[ArnCount]] = {
+
+    val pipeline = Seq(
+      Aggregates.group(
+        "$identifier",
+        Accumulators.addToSet("arns", "$arn")
+      ),
+      Aggregates.project(
+        Projections.fields(
+          Projections.computed("identifier", "$_id"),
+          Projections.computed("arnCount", Document("""{ "$size": "$arns" }"""))
+        )
+      ),
+      Aggregates.`match`(Filters.gt("arnCount", 1))
+    )
+
+    collection
+      .aggregate[Document](pipeline) // returns BSON documents
+      .toFuture()
+      .map(_.map { doc =>
+        ArnCount(
+          identifier = doc.getString("identifier"),
+          arnCount = doc.getInteger("arnCount")
+        )
+      })
+  }
+
+  private def runDuplicateArnScan(): Future[Unit] = {
+    val tasks = mappingRepositories.map { repo =>
+      findIdentifierArnCounts(repo.collection).map { results =>
+        val distribution: Map[Int, Int] =
+          results
+            .groupBy(_.arnCount)
+            .view
+            .mapValues(_.size)
+            .toMap
+
+        logger.warn(
+          s"Duplicate ARN distribution in '${repo.collectionName}': " +
+            (if (distribution.isEmpty)
+               "no duplicates found"
+             else
+               distribution.map { case (arnCount, count) => s"$count identifiers have $arnCount ARNs" }.mkString(", "))
+        )
+      }
+    }
+
+    Future.sequence(tasks).map(_ => ())
+  }
+
+}
---
+++ it/test/uk/gov/hmrc/agentmapping/service/DuplicateArnScanServiceISpec.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentmapping.service
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import uk.gov.hmrc.agentmapping.repository.NewAgentCodeMappingRepository
+import uk.gov.hmrc.agentmapping.model.Arn
+import uk.gov.hmrc.agentmapping.model.ArnCount
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.BeforeAndAfterEach
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+
+class DuplicateArnScanServiceISpec
+extends AnyWordSpec
+with Matchers
+with ScalaFutures
+with BeforeAndAfterEach
+with GuiceOneAppPerSuite {
+
+  lazy val repository: NewAgentCodeMappingRepository = app.injector.instanceOf[NewAgentCodeMappingRepository]
+
+  lazy val service: DuplicateArnScanService = app.injector.instanceOf[DuplicateArnScanService]
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    repository.deleteAll().futureValue
+  }
+
+  "findIdentifierArnCounts" should {
+    "return a row for each identifier that has more than one ARN" in {
+      repository.store(Arn("ARN0001"), "X").futureValue
+      repository.store(Arn("ARN0002"), "X").futureValue
+      repository.store(Arn("ARN0003"), "Y").futureValue // single ARN → should not appear
+
+      val results: Seq[ArnCount] =
+        service
+          .findIdentifierArnCounts(repository.collection) // visible because test is in same package
+          .futureValue
+
+      results.map(_.arnCount) should contain only 2
+    }
+  }
+
+}
